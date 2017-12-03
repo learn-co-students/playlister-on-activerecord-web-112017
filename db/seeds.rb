b96a0628e6f0690305183e80ba8841b15b79a2a4
ddbrennan <ddbren@gmail.com>
2017-12-03 11:43:12 -0500

    playlister activerecord functionality

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Artist.create(name: "Dagny")
+Artist.create(name: "Chaos Chaos")
+Artist.create(name: "Kurt Vile")
+
+Genre.create(name: "Pop")
+Genre.create(name: "Alt")
+Genre.create(name: "Rock")
+
+Song.create(name: "Wearing Nothing")
+Song.create(name: "Do You Feel It?")
+Song.create(name: "Pretty Pimpin")
