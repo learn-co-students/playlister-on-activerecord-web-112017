8a8ccf2c4d30a8eb42afa442bc8eba2c70f8d61e
eal6gf <elum1234@gmail.com>
2017-12-04 15:18:54 -0500

    Done.

--- db/seed.rb
+++ db/seed.rb
@@ -1,7 +1,8 @@
+require_relative "../config/environment.rb"
 require_relative "../app/models/artist.rb"
 require_relative "../app/models/genre.rb"
 require_relative "../app/models/song.rb"
-require "ActiveRecord"
+
 
 Artist.create(name: 'Drake')
 Song.create(name: 'Hotline Bling')
