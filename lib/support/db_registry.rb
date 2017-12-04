bb1b5a8895ad04f332d0e3c1ea291fd9291f4b48
Matt McAlister <matt.mcalister93@gmail.com>
2017-12-04 14:53:32 -0500

    Done.

--- lib/support/db_registry.rb
+++ lib/support/db_registry.rb
@@ -1,5 +1,7 @@
+require_relative './connection_adapter'
+
 require 'ostruct'
-DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/playlister-test.db"), 
-  development: ConnectionAdapter.new("db/playlister-development.db"), 
+DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/playlister-test.db"),
+  development: ConnectionAdapter.new("db/playlister-development.db"),
   production: ConnectionAdapter.new("db/playlister-production.db")
   )
