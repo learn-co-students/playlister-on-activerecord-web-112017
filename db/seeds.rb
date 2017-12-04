0c34e14741328d8c63b897611600f1e63bdac2dc
ddbrennan <ddbren@gmail.com>
2017-12-04 09:47:38 -0500

    add seed task and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@ Artist.create(name: "Chaos Chaos")
 Artist.create(name: "Kurt Vile")
 
 Genre.create(name: "Pop")
-Genre.create(name: "Alt")
 Genre.create(name: "Rock")
+Genre.create(name: "Alt")
+
 
 Song.create(name: "Wearing Nothing")
 Song.create(name: "Do You Feel It?")
